5ede5059332390357ea74f4b52b768a85b15e398
Yuki091305 <yukifieri@gmail.com>
2025-12-02 13:53:31 +0700

    Initial commit

---
+++ locallibrary/catalog/views.py
@@ -0,0 +1,29 @@
+from django.views import generic
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+    linck_books = Book.objects.filter(title__icontains='Linck').count()
+    genre_books = Book.objects.filter(genre__name__iexact='Romance').count()
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'linck_books': linck_books,
+        'genre_books': genre_books,
+    }
+
+    return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+
+class BookDetailView(generic.DetailView):
+    model = Book
