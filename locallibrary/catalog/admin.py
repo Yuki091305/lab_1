b1f7ecd8ce1cdff9189c7f63777544be9e8fd33a
Yuki091305 <yukifieri@gmail.com>
2025-12-04 11:12:21 +0700

    Initial commit

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance, Language
-
 #admin.site.register(Author)
 #admin.site.register(Book)
 admin.site.register(Genre)
