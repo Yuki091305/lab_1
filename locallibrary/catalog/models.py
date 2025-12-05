a02044771089df5729329a83618cc6f87f7001a4
Yuki091305 <yukifieri@gmail.com>
2025-12-05 10:43:34 +0700

    change ooo

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -5,6 +5,7 @@ from django.db.models.functions import Lower
 import uuid
 from django.conf import settings
 from datetime import date
+from django.core.validators import MinLengthValidator
 
 
 class Genre(models.Model):
@@ -41,7 +42,7 @@ class Book(models.Model):
 
     summary = models.TextField(
         max_length=1000, help_text="Enter a brief description of the book")
-    isbn = models.CharField('ISBN', max_length=13,
+    isbn = models.CharField('ISBN', max_length=13, validators=[MinLengthValidator(5)],
                             unique=True,
                             help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
                                       '">ISBN number</a>')
